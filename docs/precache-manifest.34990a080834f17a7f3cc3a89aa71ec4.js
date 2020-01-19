3dcd792d87fa25e058bcb6969c85214a547ac5ad
Alex <sashafedun46@gmail.com>
2020-01-20 00:01:37 +0200

    first build

---
+++ docs/precache-manifest.34990a080834f17a7f3cc3a89aa71ec4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ab3270f4ed39e5b8f1a744010f85c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d58d499b66592e88cb76",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6265ec4c1d6542c06ba7",
+    "url": "/static/css/main.897f32e3.chunk.css"
+  },
+  {
+    "revision": "d58d499b66592e88cb76",
+    "url": "/static/js/2.5d93ee8c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.5d93ee8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6265ec4c1d6542c06ba7",
+    "url": "/static/js/main.090dfcb0.chunk.js"
+  },
+  {
+    "revision": "a3ae24c73f7b90477b95",
+    "url": "/static/js/runtime-main.801dd740.js"
+  }
+]);
